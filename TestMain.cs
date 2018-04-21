89e31f6089b60a8855c8f1ef285bc083eb6a502d
Andrew Khmarsky <sanandy777@gmail.com>
2018-04-21 19:00:30 +0300

    Add: C# script

---
+++ TestMain.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TestMain : MonoBehaviour {
+	string[] names = { "Marta", "John", "Lexys", "Nancy" };
+	List<string> resultList = new List<string>();
+
+	void Start () {
+		Root ();
+	}
+	void Root(){
+//		List<string> query = names.Where (x => x.Contains ("a")).Select(x => x.ToUpper()).ToList ();
+
+
+		List<string> query = 
+			(
+				from n in names
+	          	where n.Contains ("a")
+	         	select n.ToUpper ()
+			).ToList ();
+
+		foreach (string element in query) {
+			Debug.Log (element);
+		}
+	}
+
+	void Technic(){
+		foreach (string element in names) {
+			if (element.Contains ("a")) {
+				resultList.Add (element.ToUpper ());
+			}
+		}
+		
+		foreach (string element in resultList) {
+			Debug.Log (element);
+		}
+	
+	}
+}
